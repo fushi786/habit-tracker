d8ab3058e0b5c5022e0426ea1c9252a45efcf0c8
fushi786 <younisfasee@outlook.com>
2025-12-01 00:24:10 +0000

    updating main page to go to login screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-export default function HomePage() {
-  return (
-    <main>
-      <h1>Habit Tracker</h1>
-    </main>
-  )
+import { redirect } from "next/navigation";
+
+export default function Page() {
+  redirect("/login");
 }
